7232dc802eb357a951c37fa5a0ab7a151d1c3adf
wusctong <wutongtree0513@gmail.com>
2026-02-06 08:52:46 +0800

    Finish code

--- Makefile
+++ Makefile
@@ -1,11 +1,10 @@
 default:
 	gcc sleep.c -o sleep
-	./sleep 3
-
+na:
+	gcc sleep.c -nostdlib -fno-stack-protector -o sleep
 nsl:
 	gcc sleep.c -nostdlib -o sleep
-	./sleep 3
-
 lc:
 	gcc sleep.c -nostdlib -lc -o sleep
+all:
 	./sleep 3
--- sleep.c
+++ sleep.c
@@ -1,9 +1,47 @@
 // Code from Valigo's Video
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#define SYS_write 1
+#define SYS_nanosleep 35
+#define SYS_exit 60
 
+typedef struct timespec {
+  long tv_sec;
+  long tv_nsec;
+} timespec;
+
+long syscall1(long number, long arg1) {
+  long result;
+  __asm__ __volatile__("syscall"
+                       : "=a"(result)
+                       : "a"(number), "D"(arg1)
+                       : "rcx", "r11", "memory");
+  return result;
+}
+long syscall2(long number, long arg1, long arg2) {
+  long result;
+  __asm__ __volatile__("syscall"
+                       : "=a"(result)
+                       : "a"(number), "D"(arg1), "S"(arg2)
+                       : "rcx", "r11", "memory");
+  return result;
+}
+long syscall3(long number, long arg1, long arg2, long arg3) {
+  long result;
+  __asm__ __volatile__("syscall"
+                       : "=a"(result)
+                       : "a"(number), "D"(arg1), "S"(arg2), "d"(arg3)
+                       : "rcx", "r11", "memory");
+  return result;
+}
+
+unsigned long slen(char *string) {
+  char *cursor = string;
+  while (*cursor) {
+    cursor++;
+  }
+  unsigned long result = cursor - string;
+  return result;
+}
 long stol(char *raw_long) {
   long result = 0;
 
@@ -15,21 +53,33 @@ long stol(char *raw_long) {
 
   return result;
 }
+void print(char *string) { syscall3(SYS_write, 1, (long)string, slen(string)); }
+void sleepsec(long seconds) {
+  timespec duration = {0};
+  duration.tv_sec = seconds;
+  syscall2(SYS_nanosleep, (long)&duration, 0);
+}
 
 int main(int argc, char *argv[]) {
-  printf("argc: %d, argv[0]: %s, argv[1]: %s\n", argc, argv[0], argv[1]);
+  // printf("argc: %d, argv[0]: %s, argv[1]: %s\n", argc, argv[0], argv[1]);
 
   if (argc != 2) {
-    printf("Usage: sleep [number]\nPause for [number] seconds");
+    print("Usage: sleep [number]\nPause for [number] seconds");
     return 1;
   }
 
   char *raw_sec = argv[1];
   long sec = stol(raw_sec);
 
-  sleep(sec);
+  sleepsec(sec);
 
-  return EXIT_SUCCESS;
+  return 0;
+}
+
+void exit(int code) {
+  syscall1(SYS_exit, code);
+  for (;;) {
+  }
 }
 
 __attribute__((naked)) void _start() {
